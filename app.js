b1707cffe0b5d0b3bf8b85efdabf2b16a7348a47
ElliotBachman <elliotbachmanx@gmail.com>
2024-12-19 22:35:39 -0600

    user item routes start

--- app.js
+++ app.js
@@ -1,8 +1,15 @@
 // Import express
 const express = require("express");
 
+// Import mongoose
+const mongoose = require("mongoose");
+
 // Create an instance of an Express app
 const app = express();
+
+// import routes
+
+const mainRouter = require("./routes/index");
 
 // Set the port from the environment variable or default to 3001
 const { PORT = 3001 } = process.env;
@@ -11,6 +18,15 @@ const { PORT = 3001 } = process.env;
 app.get("/", (req, res) => {
   res.send("Hello, Hot Reload!");
 });
+
+mongoose
+  .connect("mongodb://127.0.0.1:27017/wtwr_db")
+  .then(() => {
+    console.log("connected to DB");
+  })
+  .catch(console.error);
+
+app.use("/", mainRouter);
 
 // Start the server
 app.listen(PORT, () => {
