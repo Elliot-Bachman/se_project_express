ab0f4651b76babc539ee23de36170c8c977c20e9
ElliotBachman <elliotbachmanx@gmail.com>
2024-12-21 15:18:52 -0600

    typo

--- models/user.js
+++ models/user.js
@@ -10,7 +10,7 @@ const userSchema = new mongoose.Schema({
   },
   avatar: {
     type: String,
-    required: [true, "The avatar field isrequired."],
+    required: [true, "The avatar field is required."],
     validate: {
       validator(value) {
         return validator.isURL(value);
@@ -20,4 +20,5 @@ const userSchema = new mongoose.Schema({
   },
 });
 
-module.export = mongoose.model("user", userSchema);
+// Correct export
+module.exports = mongoose.model("user", userSchema);
