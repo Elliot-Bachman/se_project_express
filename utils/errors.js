3c8732e9e333a35a479940128caf3ba101ab7658
ElliotBachman <elliotbachmanx@gmail.com>
2024-12-21 22:30:35 -0600

    other tests

--- utils/errors.js
+++ utils/errors.js
@@ -6,8 +6,8 @@ const ERROR_CODES = {
 
 const ERROR_MESSAGES = {
   BAD_REQUEST: "Bad request in data or syntax.",
-  NOT_FOUND: "Page resources not found.",
-  SERVER_ERROR: "Internal Server error.",
+  NOT_FOUND: "The request was sent to a non-existent address.",
+  SERVER_ERROR: "An error has occured on the server.",
 };
 
 module.exports = { ERROR_CODES, ERROR_MESSAGES };
