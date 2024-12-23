0c8cfa47a7dbeb04087f20d541a3faa1efda233b
ElliotBachman <elliotbachmanx@gmail.com>
2024-12-23 12:38:12 -0600

    Unknown route handler fix

--- routes/index.js
+++ routes/index.js
@@ -1,11 +1,15 @@
 const router = require("express").Router();
 const userRouter = require("./users");
 const clothingItem = require("./clothingItems");
+const { ERROR_CODES, ERROR_MESSAGES } = require("../utils/errors");
 
+// Route handlers
 router.use("/users", userRouter);
 router.use("/items", clothingItem);
+
+// Middleware for handling unknown routes
 router.use((req, res) => {
-  res.status(500).send({ message: "Invalid route. Router not found" });
+  res.status(ERROR_CODES.NOT_FOUND).send({ message: ERROR_MESSAGES.NOT_FOUND });
 });
 
 module.exports = router;
