dd6813ed7690d19a089a3f3befedc8956c0381ad
ElliotBachman <elliotbachmanx@gmail.com>
2025-01-07 10:37:32 -0600

    Extra reviewer correction

--- controllers/clothingItems.js
+++ controllers/clothingItems.js
@@ -1,28 +1,6 @@
 const ClothingItem = require("../models/clothingItem");
 const { ERROR_CODES, ERROR_MESSAGES } = require("../utils/errors");
-
-// Centralized error handler
-const handleError = (err, res) => {
-  console.error(err);
-  if (err.name === "ValidationError") {
-    return res
-      .status(ERROR_CODES.BAD_REQUEST)
-      .send({ message: ERROR_MESSAGES.BAD_REQUEST });
-  }
-  if (err.name === "CastError") {
-    return res
-      .status(ERROR_CODES.BAD_REQUEST)
-      .send({ message: ERROR_MESSAGES.INVALID_ID_FORMAT });
-  }
-  if (err.name === "DocumentNotFoundError") {
-    return res
-      .status(ERROR_CODES.NOT_FOUND)
-      .send({ message: ERROR_MESSAGES.NOT_FOUND });
-  }
-  return res
-    .status(ERROR_CODES.SERVER_ERROR)
-    .send({ message: ERROR_MESSAGES.SERVER_ERROR });
-};
+const handleError = require("../utils/errorHandler");
 
 // Create a clothing item
 const createItem = (req, res) => {
